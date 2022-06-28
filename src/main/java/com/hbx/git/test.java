35e703651c4df0ffe950f069034f03106c871b3d
ham <1499091922@qq.com>
2022-06-28 19:40:45 +0800

    first commit

---
+++ src/main/java/com/hbx/git/test.java
@@ -0,0 +1,11 @@
+package com.hbx.git;
+
+/**
+ * @author 黄柏轩
+ * @version 1.0
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+}
