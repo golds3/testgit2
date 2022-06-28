dc0fcef01cdf36d5530951e67cf1964f70e6438e
ham <1499091922@qq.com>
2022-06-28 19:50:00 +0800

    hotfix comi -1

--- src/main/java/com/hbx/git/test.java
+++ src/main/java/com/hbx/git/test.java
@@ -8,6 +8,6 @@ public class test {
     public static void main(String[] args) {
         System.out.println("hello git");
         System.out.println("hello git2");
-        System.out.println("hello git3");
+        System.out.println("hello git8");
     }
 }
